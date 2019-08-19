a77dd9f69b8741bbfedeb7d56c22107086f5b7e4
USER <kimeee45@gmail.com>
2019-08-19 18:41:45 +0900

    190819 - dfs 11403, 11724

--- src/backjun/m11403.java
+++ src/backjun/m11403.java
@@ -2,56 +2,70 @@ package backjun;
 
 import java.util.*;
 public class m11403 {
+	
+	static LinkedList<Integer> node[];
+	static int result[][];
+	//static boolean visited[];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
 		
-		LinkedList<Integer> queue[] = new LinkedList[n+1];
+		node = new LinkedList[n+1];
+		result = new int[n+1][n+1];
 		
 		// [a][b]
 		for(int a=1; a<=n; a++)
 		{
-			queue[a] = new LinkedList<Integer>();
+			node[a] = new LinkedList<Integer>();
 			for(int b=1; b<=n; b++)
 			{
 				int num = sc.nextInt();
 				
+				// 1일 때,입
 				if(num == 1)
-				{
-					queue[a].add(b);
-				}
+					node[a].add(b);
+				
 			}
 		}
 		
 		for(int a=1; a<=n; a++)
 		{
-			boolean visited[] = new boolean[n+1];
-			
-			
+			for(int b=0; b<node[a].size(); b++)
+			{
+				boolean visited[] = new boolean[n+1];
+				dfs(a, node[a].get(b) , visited);
+			}
 		}
 		
-		// 데이터가 잘 담겼는지
-		/*
+		//	출력 
 		for(int a=1; a<=n; a++)
 		{
-			System.out.print(a+" : ");
-			while(!queue[a].isEmpty())
+			for(int b=1; b<=n; b++)
 			{
-				System.out.print(queue[a].poll());
+				System.out.print(result[a][b]+" ");
 			}
 			System.out.println();
 		}
-		*/
+		
+		
+		
 	}
 	
-	public static void dfs(int startEdge, int findEdge , int size, boolean visited[])
+	public static void dfs(int kijun , int next , boolean[] visited)
 	{
-		visited[startEdge] = true;
-		
-		for(int i=1; i<=size; i++)
+		if(!visited[next])
 		{
+			visited[next] = true;
+			result[kijun][next] = 1;
+			//System.out.println(kijun+"/"+next+" = 1");
 			
+			for(int i=0; i<node[next].size(); i++)
+			{
+				dfs(kijun, node[next].get(i) , visited);
+			}
 		}
 	}
+	
+	
 }
