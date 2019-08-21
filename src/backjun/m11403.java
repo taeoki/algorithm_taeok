8ead86874d14ac63f13d02a02cfc1e64d80cfde6
USER <USER@AD01311294>
2019-08-21 14:16:41 +0900

    s

--- src/backjun/m11403.java
+++ src/backjun/m11403.java
@@ -2,58 +2,70 @@ package backjun;
 
 import java.util.*;
 public class m11403 {
-	static LinkedList<Integer> queue[];
-	static int visited[];
 	
+	static LinkedList<Integer> node[];
+	static int result[][];
+	//static boolean visited[];
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
 		
-		queue = new LinkedList[n+1];
-		visited = new int[n+1];
+		node = new LinkedList[n+1];
+		result = new int[n+1][n+1];
 		
 		// [a][b]
 		for(int a=1; a<=n; a++)
 		{
-			queue[a] = new LinkedList<Integer>();
+			node[a] = new LinkedList<Integer>();
 			for(int b=1; b<=n; b++)
 			{
 				int num = sc.nextInt();
 				
+				// 1일 때,입
 				if(num == 1)
-				{
-					queue[a].add(b);
-				}
+					node[a].add(b);
+				
 			}
 		}
 		
+		for(int a=1; a<=n; a++)
+		{
+			for(int b=0; b<node[a].size(); b++)
+			{
+				boolean visited[] = new boolean[n+1];
+				dfs(a, node[a].get(b) , visited);
+			}
+		}
+		
+		//	출력 
 		for(int a=1; a<=n; a++)
 		{
 			for(int b=1; b<=n; b++)
 			{
-				dfs(a,b);
-			}
-		}
-		
-
-		// 데이터가 잘 담겼는지
-		/*
-		for(int a=1; a<=n; a++)
-		{
-			System.out.print(a+" : ");
-			while(!queue[a].isEmpty())
-			{
-				System.out.print(queue[a].poll());
+				System.out.print(result[a][b]+" ");
 			}
 			System.out.println();
 		}
-		*/
-	}
-	
-	public static void dfs(int from , int to)
-	{
+		
+		
 		
 	}
+	
+	public static void dfs(int kijun , int next , boolean[] visited)
+	{
+		if(!visited[next])
+		{
+			visited[next] = true;
+			result[kijun][next] = 1;
+			//System.out.println(kijun+"/"+next+" = 1");
+			
+			for(int i=0; i<node[next].size(); i++)
+			{
+				dfs(kijun, node[next].get(i) , visited);
+			}
+		}
+	}
+	
+	
 }
